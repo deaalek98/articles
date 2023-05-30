d9cad922433a08971ea85ef68229433ead5b8ee2
Deaa <deaa.lek98@gmail.com>
2023-05-30 11:56:10 +0400

    Bug fixes

--- lib/src/features/details/details/presentation/pages/details_page.dart
+++ lib/src/features/details/details/presentation/pages/details_page.dart
@@ -37,7 +37,9 @@ class _DetailsPageState extends State<DetailsPage> {
           SliverAppBar(
             expandedHeight: MediaQuery.of(context).size.height * 0.43,
             elevation: 0,
-            iconTheme: IconThemeData(color: Theme.of(context).primaryColor,),
+            iconTheme: IconThemeData(
+              color: Theme.of(context).primaryColor,
+            ),
             flexibleSpace: FlexibleSpaceBar(
                 collapseMode: CollapseMode.parallax,
                 background: ImageNetworkWidget(
@@ -45,9 +47,27 @@ class _DetailsPageState extends State<DetailsPage> {
                     radius: 0,
                     height: SizeConfig.h(280),
                     url: ((widget.articleModel.media?.length ?? 0) > 0) &&
-                            ((widget.articleModel.media?[widget.articleModel.media!.length - 1].metadataList?.length ?? 0) > 0)
-                        ? (widget.articleModel.media?[widget.articleModel.media!.length - 1]
-                                .metadataList?[widget.articleModel.media![widget.articleModel.media!.length - 1].metadataList!.length - 1].url ??
+                            ((widget
+                                        .articleModel
+                                        .media?[
+                                            widget.articleModel.media!.length -
+                                                1]
+                                        .metadataList
+                                        ?.length ??
+                                    0) >
+                                0)
+                        ? (widget
+                                .articleModel
+                                .media?[widget.articleModel.media!.length - 1]
+                                .metadataList?[widget
+                                        .articleModel
+                                        .media![
+                                            widget.articleModel.media!.length -
+                                                1]
+                                        .metadataList!
+                                        .length -
+                                    1]
+                                .url ??
                             "")
                         : "")),
           ),
@@ -66,44 +86,51 @@ class _DetailsPageState extends State<DetailsPage> {
                       scrollDirection: Axis.horizontal,
                       child: Row(
                           children: widget.articleModel.adxKeywords
-                              ?.split(";")
-                              .map(
-                                (e) => Container(
-                              margin: EdgeInsets.only(left: 2),
-                              child: FilterChip(
-                                // backgroundColor: AppColors.lightGrey,
-                                onSelected: (val) {},
-                                backgroundColor: Colors.black.withOpacity(0.6),
-                                labelPadding: EdgeInsets.only(left: 3, right: 3),
-                                padding: EdgeInsets.all(2),
-                                labelStyle: TextStyle(
-                                  color: Colors.white,
-                                ),
-                                shape: StadiumBorder(side: BorderSide()),
-                                side: BorderSide(color: Colors.transparent),
-                                label: Text(
-                                  e,
-                                  style: AppTextStyle.regular(sizeText: SizeText.size10).copyWith(color: Colors.white),
-                                ),
-                              ),
-                            ),
-                          )
-                              .toList() ??
+                                  ?.split(";")
+                                  .map(
+                                    (e) => Container(
+                                      margin: EdgeInsets.only(left: 2),
+                                      child: FilterChip(
+                                        // backgroundColor: AppColors.lightGrey,
+                                        onSelected: (val) {},
+                                        backgroundColor:
+                                            Colors.black.withOpacity(0.6),
+                                        labelPadding:
+                                            EdgeInsets.only(left: 3, right: 3),
+                                        padding: EdgeInsets.all(2),
+                                        labelStyle: TextStyle(
+                                          color: Colors.white,
+                                        ),
+                                        shape:
+                                            StadiumBorder(side: BorderSide()),
+                                        side: BorderSide(
+                                            color: Colors.transparent),
+                                        label: Text(
+                                          e,
+                                          style: AppTextStyle.regular(
+                                                  sizeText: SizeText.size10)
+                                              .copyWith(color: Colors.white),
+                                        ),
+                                      ),
+                                    ),
+                                  )
+                                  .toList() ??
                               []),
                     ),
-
                     ListTile(
                       horizontalTitleGap: 0,
                       dense: true,
                       visualDensity: VisualDensity(horizontal: 0, vertical: -4),
-                      contentPadding: EdgeInsets.symmetric(horizontal: 0.0, vertical: 0.0),
+                      contentPadding:
+                          EdgeInsets.symmetric(horizontal: 0.0, vertical: 0.0),
                       trailing: Text(
-                        HelperUtil.instance.getSections(widget.articleModel.updated ?? ""),
+                        HelperUtil.instance
+                            .getSections(widget.articleModel.updated ?? ""),
                         style: TextStyle(fontSize: 12, color: Colors.grey),
                       ),
                       leading: Icon(
                         Icons.article_outlined,
-                        color: Theme.of(context).accentColor,
+                        color: AppColors.deepRed,
                       ),
                     ),
                     Text(
@@ -125,6 +152,5 @@ class _DetailsPageState extends State<DetailsPage> {
         ],
       ),
     );
-
   }
 }
